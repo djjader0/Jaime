e5627dd1e10fdd290b491231477f3cd26874c191
djjader0 <djjader96@gmail.com>
2024-12-02 18:14:42 +0100

    desencriptar.php

--- desencriptar.php
+++ desencriptar.php
@@ -19,6 +19,6 @@
         echo "Mensaje Desencriptado: " . $mensajedesencriptado . "<br>";
     }
     ?>
-    <br><a href="index.php">Volver a la página principal</a>
+    <br><a href="index.html">Volver a la página principal</a>
 </body>
 </html>
